c8c2ad384c72e0d30f6c93efa5863114379a8cdb
fenilprajapati24 <fenilprajapati9334@gmail.com>
2025-07-02 11:24:28 +0530

    Update cartrouter.js

--- backend/routes/cartrouter.js
+++ backend/routes/cartrouter.js
@@ -1,32 +1,10 @@
 const express=require('express');
 const router=express.Router();
 
-// const cart=require('../models/cartmodel');
+
 const Cart = require('../models/cartmodel');
 
-// const {getcart}=require('../controllers/cartcontroller'); 
 
-// router.post('/cart',async(req,res)=>{
-
-
-//     console.log("cart of ans ");
-//     console.log("Request body:", req.body);
-//    try{
- 
-// const { cartItems } = req.body; // capital I to match frontend
-
-//   if(!cartItems || !Array.isArray(cartItems)){
-//     return res.status(400).json({error:"invalid cart data"});
-//   }
-//     const newcart=({cartItems});
-//     const savedcart=await newcart.save();
-//     res.status(200).json({message:"cart saved succesfully ",cart:savedcart});
-//    }catch(err)
-//    {
-//        console.log("err savig cart",err);
-//        res.status(500).json({error:"failed to save cart"});
-//    };
-// });
 
 router.post('/cart', async (req, res) => {
   console.log("POST /cart called with body:", req.body);
